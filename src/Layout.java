36c96663378c6d46296d654f562a92cd10fc2e3a
Brian Murray <brianmurraysound@gmail.com>
2023-02-25 14:57:42 -0400

    Added visibility toggle

--- src/Layout.java
+++ src/Layout.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Layout extends JFrame {
+public class Layout implements ActionListener {
 
     private JFrame frame;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton button1;
+    private JButton button2;
 
     public Layout() {
         frame = new JFrame("Take a swing!");
 
-        JButton button1 = new JButton("Toggle Left");
+        button1 = new JButton("Toggle Left");
         button1.setVisible(true);
         button1.setBounds(150, 0, 100, 40);
+        button1.addActionListener(this);
 
-        JButton button2 = new JButton("Toggle Right");
+        button2 = new JButton("Toggle Right");
         button2.setVisible(true);
         button2.setBounds(550, 0, 100, 40);
+        button2.addActionListener(this);
 
         JLabel label1 = new JLabel("This is the left panel");
         label1.setVisible(true);
@@ -27,13 +35,13 @@ public class Layout extends JFrame {
         label2.setFont(new Font("roboto", Font.PLAIN, 20));
 
 
-        JPanel panel1 = new JPanel();
+        panel1 = new JPanel();
         panel1.setVisible(true);
         panel1.setBounds(0, 40, 400, 360);
         panel1.setBackground(Color.blue);
         panel1.add(label1);
 
-        JPanel panel2 = new JPanel();
+        panel2 = new JPanel();
         panel2.setVisible(true);
         panel2.setBounds(400, 40, 400, 360);
         panel2.setBackground(Color.red);
@@ -47,5 +55,18 @@ public class Layout extends JFrame {
         this.frame.setSize(800, 400);
         this.frame.setVisible(true);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleVisible(e);
+    }
+
+    public void toggleVisible(ActionEvent e) {
+        if(e.getSource() == button1) {
+            this.panel1.setVisible(!this.panel1.isVisible());
+        } else if (e.getSource() == button2) {
+            this.panel2.setVisible(!this.panel2.isVisible());
+        }
     }
 }
